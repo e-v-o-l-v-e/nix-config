a2b6c2bd18d82bfdbadd056b6e0139941b579870
evolve <elivanoa5@gmail.com>
2026-02-26 21:54:31 +0100

    templates: update W41

--- modules/_templates/web-bun-express-prisma/flake.nix
+++ modules/_templates/web-bun-express-prisma/flake.nix
@@ -18,8 +18,12 @@
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
 
       bun-dep = "bun-install-dependencies";
-      db-reset-1 = "db-reset-push";
-      db-reset-2 = "db-reset-push-hard";
+      db-reset-1 = "db-reseed";
+      db-reset-2 = "db-reseed-force";
+
+      abbr-1 = "re";
+      abbr-2 = "ref";
+      set-abbr = "set-abbr";
     in
     {
       packages = forAllSystems (
@@ -30,13 +34,11 @@
         in
         {
           ${db-reset-1} = pkgs.writeShellScriptBin db-reset-1 ''
-            ${getExe' pkgs.bun "bunx"} prisma generate
-            ${getExe' pkgs.bun "bunx"} prisma db push
+            ${getExe' pkgs.bun "bunx"} prisma migrate reset
             ${getExe' pkgs.bun "bunx"} prisma db seed 
           '';
           ${db-reset-2} = pkgs.writeShellScriptBin db-reset-2 ''
-            ${getExe' pkgs.bun "bunx"} prisma generate
-            ${getExe' pkgs.bun "bunx"} prisma db push --force-reset
+            ${getExe' pkgs.bun "bunx"} prisma migrate reset -f
             ${getExe' pkgs.bun "bunx"} prisma db seed 
           '';
           ${bun-dep} = pkgs.writeShellScriptBin bun-dep ''
@@ -49,6 +51,16 @@
             ${getExe pkgs.bun} install validator
             ${getExe pkgs.bun} install -D @types/validator
           '';
+
+          ${set-abbr} = pkgs.runCommand "fish-functions" { } ''
+            mkdir -p $out/share/fish/vendor_functions.d
+            echo "function ${set-abbr}
+              abbr -a ${abbr-1} 'bunx prisma migrate reset; bunx prisma db seed'
+              abbr -a ${abbr-2} 'bunx prisma migrate reset -f; bunx prisma db seed'
+              echo '✅ fish abbreviations [${abbr-1}, ${abbr-2}] have been set for this session.'
+            end" > $out/share/fish/vendor_functions.d/${set-abbr}.fish
+          '';
+
           default = self.packages.${system}.${bun-dep};
         }
       );
@@ -64,6 +76,7 @@
             self.packages.${system}.${bun-dep}
             self.packages.${system}.${db-reset-1}
             self.packages.${system}.${db-reset-2}
+            self.packages.${system}.${set-abbr}
           ];
         in
         {
@@ -95,8 +108,18 @@
 
               echo ""
               echo "🛠️ W41 nix development shell"
-              echo "init bun libraries etc with '${bun-dep}'"
-              echo "db push reset scripts available as: [ ${db-reset-1}, ${db-reset-2} ]"
+              echo ""
+              echo "[Scripts]"
+              echo " - '${bun-dep}': install all dependencies with bun"
+              echo " - '${db-reset-1}': reset db to last migration"
+              echo " - '${db-reset-2}': force reset db to last migration"
+              echo ""
+              echo "[Shell]"
+              echo "If you use fish you can use 'nix develop --command fish'"
+              echo "and run ${set-abbr} to get the previous scripts as abbreviations: "
+              echo " - '${abbr-1}': ${db-reset-1}"
+              echo " - '${abbr-2}': ${db-reset-2}"
+              echo "run 'nix develop --command fish -C ${set-abbr}' to get abbr from the start"
             '';
           };
         }
