7c6af08174f90d09d35d6737c40fe380b1542eca
evolve <elivanoa5@gmail.com>
2026-02-26 12:27:39 +0100

    fix missing ;}

--- system/services/seerr.nix
+++ system/services/seerr.nix
@@ -4,16 +4,18 @@
   config,
   username,
   ...
-}: let
+}:
+let
   cfg = config.server;
   fqdn = cfg.domain;
   listenPort = 5055;
   version = "latest";
-in {
+in
+{
   config = {
     virtualisation.oci-containers.containers = {
       seerr = lib.mkIf cfg.docker.seerr.enable {
-	podman.user = cfg.serverGroupName;
+        podman.user = cfg.serverGroupName;
 
         autoStart = true;
         serviceName = "docker-seerr";
@@ -21,27 +23,31 @@ in {
         pull = "newer";
         image = "ghcr.io/seerr-team/seerr:${version}";
 
-        ports = ["${toString listenPort}:5055"];
+        ports = [ "${toString listenPort}:5055" ];
         volumes = [
           "${cfg.configPath}/seerr:/app/config"
         ];
 
-        extraOptions = [ "--network=host" "--userns=keep-id:uid=1000,gid=1000" ];
+        extraOptions = [
+          "--network=host"
+          "--userns=keep-id:uid=1000,gid=1000"
+        ];
       };
     };
 
     services.caddy.virtualHosts = lib.mkIf cfg.docker.seerr.enable {
       "seerr.${fqdn}" = {
         extraConfig = ''
-          import cfdns
-	  reverse_proxy http://localhost:${toString listenPort}
-      '';
+                    import cfdns
+          	  reverse_proxy http://localhost:${toString listenPort}
+        '';
       };
     };
 
     systemd.services."docker-seerr" = {
       stopIfChanged = false;
     };
+  };
 
   options = {
     server.docker.seerr.enable = lib.mkEnableOption "Enable seerr docker container (package is kinda broken rn)";
